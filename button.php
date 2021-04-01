ed17966f8d6f12f2bf3cf62d301fe7e32c40c7ef
Sanjeev Kumar <amsanjeevkumar@gmail.com>
2021-04-01 23:21:11 +0530

    Initial Commit

---
+++ button.php
@@ -0,0 +1,99 @@
+<?php
+if (isset($_POST['on']))
+{
+print "
+<html>
+<body>
+<title>Home Automation</title>
+<style type=text/css>
+h1{
+	padding-left: 300px;
+}
+h2{
+	position: absolute;
+	top: 100px;
+	left: 450px;
+}
+</style>
+<h1>DIY Hacking - Internet of Things Implementation</h2>
+<h2>The Device has been Turned ON </h2>
+</body>
+</html>
+";
+
+$servername = "localhost";
+$username = "db_user";
+$password = "Passw0rd";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE status SET current_state='ON' where button='0'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<center>Button Status recorded</center>";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+}
+else if(isset($_POST['off']))
+{
+print "
+<html>
+<body>
+<title>Home Automation</title>
+<style type=text/css>
+h1{
+	padding-left: 300px;
+}
+h2{
+	position: absolute;
+	top: 100px;
+	left: 450px;
+}
+</style>
+<h1>DIY Hacking - Internet of Things Implementation</h2>
+<h2>The Device has been Turned OFF </h2>
+</body>
+</html>
+";
+
+$servername = "localhost";
+$username = "db_user";
+$password = "Passw0rd";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE status SET current_state='OFF' where button='0'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<center>Button Status recorded</center>";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+}
+
+$command = escapeshellcmd("sudo python /var/www/html/rasbpi.py.new");
+
+$output = shell_exec($command);
+
+?>
---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "db_user";
+$password = "Passw0rd";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO status VALUES ('2','OFF')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
